f9683036f27f17aef9797a1fe9a84db461f34a0e
mayankcodeops <mayank@codeops.tech>
2021-12-07 15:00:44 +0530

    Flattening JSON refactoring

--- generate_csv.py
+++ generate_csv.py
@@ -1,35 +1,49 @@
-import requests
 import csv
-import pdfkit
+
 import pandas as pd
+import pdfkit
+import requests
+
 BASE_URL = 'https://collectionapi.metmuseum.org/public/'
 LIMIT = 20
 
 headers = {
-    'Accept' : '*/*',
-    'Content-Type' : 'application/json'
+    'Accept': '*/*',
+    'Content-Type': 'application/json'
 }
 
 objects = []
 
-def fetch_artifact(object_id):
-        response_object = requests.get(BASE_URL + 'collection/v1/objects/' + str(object_id),headers=headers)
-        return response_object.json()
+
+def fetch_artifact(objectID):
+    response_object = requests.get(BASE_URL + 'collection/v1/objects/' + str(objectID), headers=headers)
+    return response_object.json()
 
 
-for object_id in range(1,LIMIT+1):
-    objects.append(fetch_artifact(object_id))
+for objectID in range(1, LIMIT + 1):
+    objects.append(fetch_artifact(objectID))
 
 
+def flatten(artifact):
+    try:
+        for constituent in artifact['constituents']:
+            for key, value in constituent.items():
+                artifact[key] = value
+            artifact.pop('constituents')
+    except TypeError:
+        pass
+
+for artifact in objects:
+    flatten(artifact)
 
 df = pd.DataFrame(objects)
 
-#generate csv
-df.to_csv('museum.csv', mode = 'w', index = False)
-#generate html
+# generate csv
+df.to_csv('museum.csv', mode='w', index=False)
+# generate html
 df.to_html('museum.html')
-#generate pdf report
-pdfkit.from_file('museum.html','museum.pdf')
+# generate pdf report
+pdfkit.from_file('museum.html', 'museum.pdf')
 
 # converting csv to xml
 f = open('museum.csv')
@@ -41,7 +55,8 @@ for row in museum_csv:
 
 f.close()
 
-#print(data[1:])
+
+# print(data[1:])
 def convert_row(row):
     return """
     <object>
@@ -55,7 +70,6 @@ def convert_row(row):
         <title>%s</title>
     </object>
     """ % (row[0], row[1], row[2], row[3], row[4], row[9], row[10], row[11])
-
 
 
 with open('museum.xml', 'w') as museum:
