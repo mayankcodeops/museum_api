90f8b07e4c2d09704515b34668b18f6026d6b46f
mayankcodeops <mayank@codeops.tech>
2021-12-13 20:13:23 +0530

    Added Class Documentation for Converter

--- src/reporter/converter/converter.py
+++ src/reporter/converter/converter.py
@@ -8,16 +8,43 @@ class Converter:
     """
     Converter class to wrap other report converter classes
     """
-    def convert_to_csv(self, directory, filename, df):
+    @staticmethod
+    def convert_to_csv(directory, filename, df):
+        """
+        Converts dataframe to CSV report
+        :param directory: directory path for creating CSV report
+        :param filename: name of the CSV report to be generated
+        :param df: Pandas Dataframe Object
+        """
         CSVConverter().convert(directory, filename, df)
 
-    def convert_to_html(self, directory, filename, df):
+    @staticmethod
+    def convert_to_html(directory, filename, df):
+        """
+        Converts dataframe object to an HTML report
+        :param directory: directory path for creating HTML report
+        :param filename: name of the HTML report to be generated
+        :param df: Pandas dataframe object
+        """
         HTMLConverter().convert(directory, filename, df)
 
-    def convert_to_pdf(self, directory, pdf_name, html_report):
+    @staticmethod
+    def convert_to_pdf(directory, pdf_name, html_report):
+        """
+        Converts HTML report to PDF format
+        :param directory: directory path for creating the PDF report
+        :param pdf_name: name of the PDF report to be generated
+        :param html_report: name of the HTML report
+        """
         PDFConverter().convert(directory, pdf_name, html_report)
 
-    def convert_to_xml(self, directory, csv_file):
+    @staticmethod
+    def convert_to_xml(directory, csv_file):
+        """
+        Converts CSV file records to XML report
+        :param directory: directory path for the XML report to be generated
+        :param csv_file: name of the CSV file to be generated
+        """
         XMLConverter().convert(directory, csv_file)
 
 
