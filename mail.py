1be9f8069b29ff1dd2c6002a2b0b9df773e9dc75
mayankcodeops <mayank@codeops.tech>
2021-12-30 15:12:48 +0530

    Refactored mail.py

--- mail.py
+++ mail.py
@@ -2,64 +2,66 @@ import os
 import datetime
 import smtplib
 import ssl
+
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import logging
-from config import config
 from jinja2 import Environment, PackageLoader, select_autoescape
+
+from config import config
+
 env = Environment(
     loader=PackageLoader("mail"),
     autoescape=select_autoescape()
 )
 
-
-CONFIG_NAME = os.environ.get('CONFIG_NAME', 'default')
-
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.DEBUG)
 
-mail_template = env.get_template("report.html")
+CONFIG_NAME = os.environ.get('CONFIG_NAME', 'default')
 
-email = config[CONFIG_NAME].ADMIN_MAIL
-password = config[CONFIG_NAME].MAIL_PASSWD
 
-from_address = email
-to_address = email
-subject = f'Museum API Reports - {datetime.datetime.now().strftime("%c")}'
-body = mail_template.render()
+def construct_message(sender, receiver, subject, body, file):
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = receiver
+    message["Subject"] = subject
 
-message = MIMEMultipart()
-message["From"] = from_address
-message["To"] = to_address
-message["Subject"] = subject
+    message.attach(MIMEText(body, "html"))
 
-message.attach(MIMEText(body, "html"))
+    try:
+        with open(file, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+    except OSError as err:
+        logging.exception(f"Error while writing attachments to payload stream: {err.args[-1]}")
 
-reports = "reports.zip"
+    encoders.encode_base64(part)
 
-try:
-    with open(reports, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-except OSError as err:
-    logging.exception(f"Error while writing attachments to payload stream: {err.args[-1]}")
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename = {file}"
+    )
 
-encoders.encode_base64(part)
+    message.attach(part)
+    return message
 
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename = {reports}"
-)
 
-message.attach(part)
-text = message.as_string()
+def send_mail(sender, receiver, subject, body, attachment=None):
+    message = construct_message(sender, receiver, subject, body, attachment)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=ssl.create_default_context()) as server:
+        server.login(email, password)
+        server.sendmail(sender, receiver, message.as_string())
 
-context = ssl.create_default_context()
 
 if __name__ == '__main__':
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(email, password)
-        server.sendmail(from_address, to_address, text)
+    email = config[CONFIG_NAME].ADMIN_MAIL
+    password = config[CONFIG_NAME].MAIL_PASSWD
+    mail_subject = f'Museum API Reports - {datetime.datetime.now().strftime("%c")}'
+    mail_template = env.get_template("report.html")
+    mail_body = mail_template.render()
+    send_mail(email, email, mail_subject, mail_body, "reports.zip")
+
 
